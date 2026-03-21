9c6f95184befb60dddfb7cca8a9743d8c781536d
Sara <sarazuluagatrujillo@gmail.com>
2026-03-21 10:44:47 -0500

    ejercicio 3

---
+++ talleres/TallerSO_03-master/bash_02/unico.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ $# -ne 2 ]; then
+    echo "Uso: $0 <nombre_fichero> <columna>"
+    exit 1
+fi
+
+fichero="$1"
+columna="$2"
+
+awk -F'|' -v col="$columna" 'NF > 0 { print $col }' "$fichero" | sort | uniq
